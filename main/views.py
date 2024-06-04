713993ad9e8ac6e2c0289bfae238f974ab55c049
volyES <qwertqwerqwerqwer777@gmail.com>
2024-06-04 10:54:09 +0300

    [start-project][#2]

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 items = [
    {"id": 1, "name": "Кроссовки abibas"},
-   {"id": 2, "name": "Куртка кожаная"},
+#    {"id": 2, "name": "Куртка кожаная"},
    {"id": 3, "name": "Coca-cola 1 литр"},
-   {"id": 4, "name": "Картофель фри"},
+#    {"id": 4, "name": "Картофель фри"},
    {"id": 5, "name": "Кепка"},
 ]
 
@@ -27,13 +27,14 @@ def about(request):
 
 def get_item(request, item_id):
     number_page = item_id
-    len_items = len(items)
-    print(len_items)
-    if int(number_page) > len_items:
-        text = f'''Товар с id={number_page} не найден'''
-        return HttpResponse(text)
-    item = str(items[int(number_page) - 1]['name']) + '<br>'
-    item += f'''<br><a href="/items">Назад к списку товаров</a>'''
+    for i in items:
+        if int(i['id']) == int(number_page):
+            item = str(i['name']) + '<br>'
+            item += '''<br><a href="/items">Назад к списку товаров</a>'''
+            return HttpResponse(item)
+        else:
+            item = (f'''Товар с id={number_page} не найден<br>
+                    <br><a href="/items">Назад к списку товаров</a>''')
     return HttpResponse(item)
 
 
@@ -41,4 +42,3 @@ def get_items(request):
     i = ([f"""<a href="{item['id']}">{item['id']}. {item['name']}</a>"""+'<br>'
           for item in items])
     return HttpResponse(i)
-
